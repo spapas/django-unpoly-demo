c8a7f56a5a0d85edfde09d6f6b05ea7e39209f6e
Serafeim Papastefanos <spapas@gmail.com>
2023-03-27 21:37:32 +0300

    Fix rsp

--- unpoly_demo/settings.py
+++ unpoly_demo/settings.py
@@ -60,8 +60,11 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
         "OPTIONS": {
+            'loaders': [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ],
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
