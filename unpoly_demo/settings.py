e68117b12fce1083f66ddd1b57a4ea805a5a2ca7
Serafeim Papastefanos <spapas@gmail.com>
2023-03-30 10:45:10 +0300

    Allowed hosts

--- unpoly_demo/settings.py
+++ unpoly_demo/settings.py
@@ -22,10 +22,11 @@ SECRET_KEY = os.environ.get(
 # DEBUG = "RENDER" not in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['unpoly-demo.spapas.net']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    
 
 # Application definition
 
